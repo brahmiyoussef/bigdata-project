2233227ad36eb5d0e2eb80555ac951662b7a4aeb
youssef <youssefbrahmi03@gmail.com>
2024-11-13 10:04:30 +0100

    changed the endpoint to the market screen

--- data_ingestion/yahoo_api.py
+++ data_ingestion/yahoo_api.py
@@ -1,42 +1,38 @@
 import requests
 import time
-from kafka import KafkaProducer
+from confluent_kafka import Producer
 import json
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-# Define the API endpoint and parameters
-url = "https://yahoo-finance-api-data.p.rapidapi.com/summary/option-price"
-querystring = {"symbol": "AAPL"}
-
+url = "https://yahoo-finance15.p.rapidapi.com/api/v1/markets/screener"
+querystring = {"list": "day_gainers"}
 headers = {
-    "x-rapidapi-key": "e44d9f362bmsh68963b8873fe8a6p1b7348jsn8dd1f6dfefce",
-    "x-rapidapi-host": "yahoo-finance-api-data.p.rapidapi.com"
+    "x-rapidapi-key": os.getenv("RAPIDAPI_KEY"),
+    "x-rapidapi-host": "yahoo-finance15.p.rapidapi.com"  # double-check this value
 }
 
-# Initialize Kafka producer
-producer = KafkaProducer(
-    bootstrap_servers='localhost:9092',  # Update with your Kafka server address
-    value_serializer=lambda v: json.dumps(v).encode('utf-8')
-)
+conf = {'bootstrap.servers': 'localhost:9092'}
+producer = Producer(conf)
+topic_name = 'market_screen'
+def delivery_report(err, msg):
+    if err is not None:
+        print(f"Message delivery failed: {err}")
+    else:
+        print(f"Message delivered to {msg.topic()} [{msg.partition()}]")
 
-# Kafka topic name
-topic_name = 'aapple'
-
-# Loop to send requests at a rate of 15 per second
 while True:
     try:
-        # Make the API request
-        response = requests.get(url, headers=headers, params=querystring)
-        data = response.json()
-        print('got data from api')
-
-        # Send data to Kafka
-        producer.send(topic_name, value=data)
-        producer.flush()
-        print("Sent data to Kafka:")
-
-        # Wait to maintain 15 requests per second rate
-        time.sleep(0.0667)  # 1/15 seconds
-
+        response = requests.get(url, headers=headers, params=querystring)  # Removed verify=False for SSL check
+        if response.status_code != 200:
+            print(f"Error: {response.status_code} - {response.text}")
+        else:
+            print('Got data from API')
+            data = response.json()
+            producer.produce(topic_name, value=json.dumps(data), callback=delivery_report)
+            producer.flush()
+            time.sleep(0.0667)
     except Exception as e:
         print("Error:", e)
-        time.sleep(1)  # Wait a bit before retrying in case of error
+        time.sleep(1)
